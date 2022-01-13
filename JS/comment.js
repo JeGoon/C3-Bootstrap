0e96ad2c1b1b3d971f997fa8c9ea42f5b0f97184
Thomas van Hoek <73465942+DeMfingThomas@users.noreply.github.com>
2022-01-13 11:10:57 +0100

    dadadw

--- JS/comment.js
+++ JS/comment.js
@@ -1,50 +1,7 @@
+$(document).ready(function(){
+  $("#commentButton").click(function(){
+    let invoer = $("#cname").val();
+    vak1.push(invoer+"<br>");
 
-function render(data) {
-  var html=
-  '<div class="commentBox" class="class="col-lg-12 col-xm-4 col-md-6 bg-warning">'+
-    '<div class="leftPanelImg">'+
-      '<img id="profile" src="img/noprofile.png" alt="Girl in a jacket" width="115px" height="110px">'+
-    '</div>'+
-    '<div class="rightPanel">'+
-      '<p class="informatie">'+
-        data.name+
-      '</p>'+
-      '<div class="date">'+
-        new Date() +
-      '</div>'+
-      '<p>'+
-        data.body+
-      '</p>'+
-    '</div>'+
-    '<div class="clear"></div>' +
-  '</div>';
-    $("#container").append(html);
-}
-$( document ).ready(function() {
-
-
-
-var comment = [
-{"name": "Look Haandrikman", "body": "Hey, Dit is een voorbeeld van een comment!"}
-];
-
-
-  for (var i = 0; i<comment.length; i++) {
-      // html +=
-      render(comment[i]);
-  }
-
-  $("#addComment").click(function(){
-    var addObj = {
-        "name": $("#name").val(),
-        "body": $("#text").val()
-    };
-
-    comment.push(addObj);
-    render(addObj);
-    $("#name").val(' ');
-    $("#text").val(' ');
-
-  })
-
+      $("#vak1").html(vak1);
 });
